0a71a4da31f6312ad4a10b451e2ef021ce35a6ff
SentoMarcos <smariba@epsg.upv.es>
2025-04-13 19:15:19 +0200

    FIX - Refactor database models and associations;

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,8 +1,8 @@
 // filepath: /home/sento/HispanoTech-UGC-WEB/config/supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://pynqyypeepucyrrmncpi.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5bnF5eXBlZXB1Y3lycm1uY3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNzY4MTUsImV4cCI6MjA1OTg1MjgxNX0.snRvFKlyvlNKRbiduOR2NfXDXeMjqn_2mvJGXSafdIY';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 let conexion = false; // Cambiado de const a let
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
